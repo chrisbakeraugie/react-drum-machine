a8ef47ce39c413c90e40c8063a816d0db4a353a0
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-01-30 20:48:50 -0500

    Updates

---
+++ precache-manifest.d0eff599f212c7ef8312528fe20060fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5c850f58e08a41200585653af742b2",
+    "url": "/react-drum-machine/index.html"
+  },
+  {
+    "revision": "916d4fc6c8674987193c",
+    "url": "/react-drum-machine/static/css/main.bd773c88.chunk.css"
+  },
+  {
+    "revision": "0cd60cf74fb69b29ac23",
+    "url": "/react-drum-machine/static/js/2.dda7a4a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-drum-machine/static/js/2.dda7a4a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "916d4fc6c8674987193c",
+    "url": "/react-drum-machine/static/js/main.2c60b20c.chunk.js"
+  },
+  {
+    "revision": "742fd48e9fb0e95fef43",
+    "url": "/react-drum-machine/static/js/runtime-main.9198a9ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-drum-machine/precache-manifest.e8f9a4ee5f11e6b1260d8db44ee6d09c.js"
+  "/react-drum-machine/precache-manifest.d0eff599f212c7ef8312528fe20060fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/random-drum-machine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-drum-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
